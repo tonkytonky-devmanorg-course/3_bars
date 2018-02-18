f41b30004c371ff0c51ea54d0c7e983e82d95e24
tonkytonky <as@asamsonov.me>
2018-02-19 02:00:08 +0300

    Refactor main logic to display the biggest, the smallest and the closest bars at once

--- bars.py
+++ bars.py
@@ -7,29 +7,27 @@ from functools import partial
 def _main():
     parser = argparse.ArgumentParser()
     args = get_args(parser)
+    longitude, latitude = args.lon, args.lat
     bars = load_json(args.filename)
 
-    if args.mode == 'b':
-        print(format_bar_output(get_biggest_bar(bars), 'Самый большой бар'))
-    elif args.mode == 's':
-        print(format_bar_output(get_smallest_bar(bars), 'Самый маленький бар'))
-    elif args.mode == 'c':
-        if not all((args.lon, args.lat)):
-            parser.error("for `c` option --lon and --lat values are required")
-
-        longitude, latitude = args.lon, args.lat
-        print(format_bar_output(get_closest_bar(bars, longitude, latitude), 'Самый близкий бар'))
+    print(format_bar_output(get_biggest_bar(bars), 'Самый большой бар'))
+    print(format_bar_output(get_smallest_bar(bars), 'Самый маленький бар'))
+    print(format_bar_output(get_closest_bar(bars, longitude, latitude), 'Самый близкий бар'))
 
 
 def get_args(parser):
-    parser.add_argument('filename', help='Path to JSON file from https://data.mos.ru/opendata/7710881420-bary')
     parser.add_argument(
-        'mode', choices=['b', 's', 'c'],
-        help='Choose the bar you want to find: `b` for biggest, `s` for smallest, `c` for closest '
-             '(--lon and --lat values are required).'
+        'filename',
+        help='Path to JSON file from https://data.mos.ru/opendata/7710881420-bary'
     )
-    parser.add_argument('--lon', help='Current longitude (float point number)', type=float, default=None)
-    parser.add_argument('--lat', help='Current latitude (float point number)', type=float, default=None)
+    parser.add_argument(
+        '--lon', help='Current longitude (float point number)',
+        type=float, recuire=True
+    )
+    parser.add_argument(
+        '--lat', help='Current latitude (float point number)',
+        type=float, recuire=True
+    )
 
     return parser.parse_args()
 
